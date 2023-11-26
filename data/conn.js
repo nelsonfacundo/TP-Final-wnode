787ab8f862809e2a422cce08db5e32be1974e8b9
Pehuenia <msaguero@gmail.com>
2023-11-26 13:05:21 -0300

    Actualización de conexión: Se cambia a puerto 5000 y se pasa link de conexión a variable de entorno.

--- data/conn.js
+++ data/conn.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const mongoclient = require("mongodb").MongoClient;
-//const uri= "mongodb+srv://noeliataboadavega:123456abc@cluster0.kn1swxq.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://admin:admin@tpintegrador.kdtimns.mongodb.net/";
+require('dotenv').config();
+const mongoclient = require('mongodb').MongoClient;
+
+const uri = process.env.MONGODB;
 const client = new mongoclient(uri);
 
 let instance = null;
@@ -15,9 +15,7 @@ async function getConnection() {
 
 async function dataAccess(database, theCollection) {
   const connectiondb = await getConnection();
-  return await connectiondb
-    .db(database)
-    .collection(theCollection);
+  return await connectiondb.db(database).collection(theCollection);
 }
 
 module.exports = { dataAccess };
