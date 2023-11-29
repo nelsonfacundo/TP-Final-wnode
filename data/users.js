b2fe79b50d08d5c1c6862d4869a55ca41cd96461
Pehuenia <msaguero@gmail.com>
2023-11-28 23:04:54 -0300

    Se agrega verificación de email en registro de usuario.

--- data/users.js
+++ data/users.js
@@ -1,46 +1,48 @@
 const conn = require("./conn");
 const bcrypt = require("bcrypt");
-const jwt =  require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const constants = require("../lib/constants");
 const errors = require("../lib/errors");
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 
 async function dataAccess() {
   return await conn.dataAccess(constants.DATABASE, constants.USERS);
 }
 
-
-// http://localhost:3000/api/pets/
+// http://localhost:5000/api/pets/
 async function getAllUsers(pageSize, page) {
-	const collection = await dataAccess();
-	const totalUsers = await collection.countDocuments();
+  const collection = await dataAccess();
+  const totalUsers = await collection.countDocuments();
 
-	const users = await collection
-		.find({})
-		.limit(pageSize)
-		.skip(pageSize * page)
-		.toArray();
-	return { totalUsers, users };
+  const users = await collection
+    .find({})
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return { totalUsers, users };
 }
 
 async function addUser(user) {
-  user.password = await bcrypt.hash(user.password, 8);
   const collection = await dataAccess();
-  const result = collection
-    .insertOne(user);
-  return result;
-}
+  const existingUser = await getUserByEmail(user.email);
 
+  if (!existingUser) {
+    user.password = await bcrypt.hash(user.password, 8);
+    const result = await collection.insertOne(user);
+
+    return result && result.acknowledged ? result : null;
+  } else {
+    throw new Error(errors.REGISTERED_EMAIL);
+  }
+}
 
 async function findByCredentials(email, password) {
   const collection = await dataAccess();
 
-  const user = await collection
-    .findOne({ email: email });
+  const user = await collection.findOne({ email: email });
   if (!user) {
     throw new Error(errors.REQUEST_ERROR);
   }
-
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
@@ -49,10 +51,15 @@ async function findByCredentials(email, password) {
   return user;
 }
 
-async function getUser(id){
+async function getUserByEmail(email) {
   const collection = await dataAccess();
-  const user = await collection
-                      .findOne({_id:new ObjectId(id)});    
+  const user = await collection.findOne({ email: email });
+  return !!user;
+}
+
+async function getUser(id) {
+  const collection = await dataAccess();
+  const user = await collection.findOne({ _id: new ObjectId(id) });
   return user;
 }
 
@@ -63,11 +70,17 @@ function generateAuthToken(user) {
       email: user.email,
       firstName: user.firstName,
       lastName: user.lastName,
-      roll: user.roll // TODO: queremos cambiar a rol?
-    }, process.env.CLAVE_SECRETA
+      roll: user.roll, // TODO: queremos cambiar a rol?
+    },
+    process.env.CLAVE_SECRETA
   );
   return token;
 }
 
-
-module.exports = { addUser, findByCredentials, generateAuthToken,getUser, getAllUsers };
+module.exports = {
+  addUser,
+  findByCredentials,
+  generateAuthToken,
+  getUser,
+  getAllUsers,
+};
